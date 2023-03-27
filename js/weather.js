1db857a934e8c6c4a751604de630cb1e3e0d26fa
wwhfree <wwhfree@qq.com>
2023-03-27 23:30:43 +0800

    update post

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "AAAAAA",
+    "tmpSize": "16",
+    "cityColor": "AAAAAA",
+    "citySize": "16",
+    "aqiColor": "AAAAAA",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "8",
+    "fixed": "false",
+    "vertical": "middle",
+    "horizontal": "left",
+    "key": "d36a5cbd68df4997ae03466c8d904dde"
+  }
+}
